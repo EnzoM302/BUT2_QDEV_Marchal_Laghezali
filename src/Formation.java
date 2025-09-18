a04cd5cc0de8ea0774f0b77dcd14328c78625b6a
nacime <nacime.laghezali3@etu.univ-lorraine.fr>
2025-09-18 10:33:05 +0200

    ajout de la class Formation

---
+++ src/Formation.java
@@ -0,0 +1,32 @@
+import java.util.HashMap;
+
+public class Formation {
+    private String id;
+    private HashMap<String, Double> matirere;
+
+    public Formation(String id, HashMap<String, Double> matirere) {
+        this.id = id;
+        this.matirere = new HashMap<>(matirere);
+    }
+
+    public void ajouterMatiere(String matiere, double coefficient) {
+        this.matirere.put(matiere, coefficient);
+    }
+
+    public void supprimerMatiere(String matiere) {
+        this.matirere.remove(matiere);
+    }
+
+    public double getCoef(String mat) {
+        if (matirere.containsKey(mat)) {
+            return matirere.get(mat).doubleValue();
+        }
+        return 0;
+    }
+
+
+
+
+
+
+}
