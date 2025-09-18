4565de7cec06fe81d9aff8951b2160751edaf000
Enzo <enzo2.marchal@gmail.com>
2025-09-18 11:11:58 +0200

    ajout d'exception personnaliser

---
+++ src/Exception/MatiereException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class MatiereException extends Exception {
+    public MatiereException(String message) {
+        super(message);
+    }
+}
---
+++ src/Exception/NoteException.java
@@ -0,0 +1,7 @@
+package Exception;
+
+public class NoteException extends Exception {
+    public NoteException(String message) {
+        super(message);
+    }
+}
